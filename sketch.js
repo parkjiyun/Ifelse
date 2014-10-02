f8b6ee80aee02f4df1b6e8fe237517473bb3df94
parkjiyun <jyun38@gmail.com>
2014-10-02 16:53:31 +0900

    DDD

--- sketch.js
+++ sketch.js
@@ -3,20 +3,43 @@ function setup()
 	createCanvas(800, 600);
 	background(0);
 	noFill();
-	stroke(255, 0, 0);
-	ellipse(400, 300, 100, 100);
+	noStroke();
 }
 
 
 function draw()
 {
-	background(255, 255, 0);
-	ellipse(400, 400, 400, 400);
+	background(255);
+	fill(255, 0, 0);
+
+	// draw function
+	if (mouseX < 100) {
+		ellipse(50, 200, 100, 100);
+	} else if (mouseX < 200){
+		ellipse(150, 200, 100, 100);
+	} else if (mouseX < 300){
+		ellipse(250, 200, 100, 100);
+	} else if (mouseX < 400){
+		ellipse(350, 200, 100, 100);
+	} else if (mouseX < 500){
+		ellipse(450, 200, 100, 100);
+	} else {
+		ellipse(550, 200, 100, 100);
+
+		fill(0,255,0);
+		noStroke();
+		if (mouseY < 100) {
+			ellipse(550, 200, 100, 100);
+		} else if (mouseY < 200){
+			ellipse(550, 300, 100, 100);
+		} else if (mouseY < 300){
+			ellipse(550, 400, 100, 100);
+		} else if (mouseY < 400){
+			ellipse(550, 500, 100, 100);
+		} 
+
+	}
 
 
-if (mouseX < 300) {
-  ellipse(200, 200, 100, 100);
-} else {
-  ellipse(400, 200, 100, 100);
-}
+
 }
