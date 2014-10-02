a5cbb707527bca4cd3e38a0c30b2093be46f221d
parkjiyun <jyun38@gmail.com>
2014-10-02 16:07:41 +0900

    ddfswef

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
-function setup() {
+function setup()
+{
+	createCanvas(800, 600);
+	background(0);
+	noFill();
+	stroke(255, 0, 0);
+	ellipse(400, 300, 100, 100);
+}
 
-  createCanvas(550, 550);
-  background(255, 255, 0);
-  stroke(0, 255, 0);
 
-  ellipse(50, 50, 50, 50); 
+function draw()
+{
+	background(255, 255, 0);
+	ellipse(400, 300, 400, 300);
+
 }
